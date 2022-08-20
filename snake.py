b82a1bc3a252b436eebbe107b2414fd05950580e
Omer Aqeel <93266569+OmerAqeel@users.noreply.github.com>
2022-08-20 19:51:35 +0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,21 +1,25 @@
 from turtle import Turtle
+
+positions = [(0, 0), (-20, 0), (-40, 0)]
+
 class Snake:
     def __init__(self):
-        positions = [(0, 0), (-20, 0), (-40, 0)]
-        global segments
-        segments = []
+        self.segments = []
         # Creating the segments of snakes
+        self.create()
+    def create(self):
         for pos in positions:
             seg = Turtle(shape="square")
             seg.penup()
             seg.color("white")
             seg.setposition(pos)
-            segments.append(seg)
+            self.segments.append(seg)
 
     # Each segment will move to the position of the segment before/infront it.
     def move(self):
-        for seg in range(len(segments) - 1, 0, -1):
-            new_x = segments[seg - 1].xcor()
-            new_y = segments[seg - 1].ycor()
-            segments[seg].goto(x=new_x, y=new_y)
-        segments[0].forward(20)
+        for seg in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg - 1].xcor()
+            new_y = self.segments[seg - 1].ycor()
+            self.segments[seg].goto(x=new_x, y=new_y)
+        self.segments[0].forward(20)
+

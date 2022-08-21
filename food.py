c977144fc3951c54b740637c29db03a709a756f2
Omer Aqeel <93266569+OmerAqeel@users.noreply.github.com>
2022-08-21 14:26:56 +0300

    Update food.py

--- food.py
+++ food.py
@@ -1,5 +1,8 @@
 from turtle import Turtle
+import random
 
+X = random.randint(-270, 270)
+Y = random.randint(-270, 270)
 class Food(Turtle):         #Inheriting from the Turtle class
 
     def __init__(self):
@@ -9,3 +12,4 @@ class Food(Turtle):         #Inheriting from the Turtle class
         self.shapesize(stretch_len= 0.5, stretch_wid= 0.5)
         self.color("red")
         self.speed("fastest")
+        self.goto(x=X,y=Y)

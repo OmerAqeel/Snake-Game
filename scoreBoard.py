d6161981f8f80aa9b81c7cb72fc45cbf65df5be3
Omer Aqeel <93266569+OmerAqeel@users.noreply.github.com>
2022-08-21 17:09:38 +0300

    Update scoreBoard.py

--- scoreBoard.py
+++ scoreBoard.py
@@ -1,11 +1,19 @@
-from turtle import Turtle
+ffrom turtle import Turtle
 
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
-        self.goto(x = -30, y = 280)
         self.score = 0
+        self.goto(x = -30, y = 280)
         self.color("white")
         self.hideturtle()
+        self.update_score()
+
+    def update_score(self):
         self.write(f"Score: {self.score}", font=("Algerian", 17, "normal"))
+
+    def giveScore(self):
+        self.score += 1     # score will increment by 1
+        self.clear()        # clears the previous score from the turtle screen to avoid overlapping
+        self.update_score() # after the previous score has been cleared the score is updated
 

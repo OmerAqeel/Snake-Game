70b19ed9901ba9852aa5da561ed64eb0784f4680
Omer Aqeel <93266569+OmerAqeel@users.noreply.github.com>
2022-08-24 01:56:38 +0300

    Update scoreBoard.py

--- scoreBoard.py
+++ scoreBoard.py
@@ -4,13 +4,22 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.get_high_score()
         self.goto(x=-130, y=270)
         self.color("white")
         self.hideturtle()
         self.update_score()
 
+    def get_high_score(self):
+        file = open("data.txt", mode="r")
+        content = file.read()
+        self.high_score = content
+        file.close()
+        return int(self.high_score)
+
     def reset(self):
+        file = open("data.txt", mode="w")
+        file.write(f"{self.high_score}")
         if self.score > self.high_score:
             self.high_score = self.score    # The final score becomes the high score if the score was greater than the all time high score
         self.score = 0                  # The score is set back to 0 soon after the game is reset

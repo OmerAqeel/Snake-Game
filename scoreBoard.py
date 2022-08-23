c1f919b23459f74b5b9185f4816ce89cfa1faa4d
Omer Aqeel <93266569+OmerAqeel@users.noreply.github.com>
2022-08-23 20:40:55 +0300

    new feature added (high score)

--- scoreBoard.py
+++ scoreBoard.py
@@ -4,16 +4,22 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.goto(x=-49, y=270)
+        self.high_score = 0
+        self.goto(x=-130, y=270)
         self.color("white")
         self.hideturtle()
         self.update_score()
 
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score    # The final score becomes the high score if the score was greater than the all time high score
+        self.score = 0                  # The score is set back to 0 soon after the game is reset
+        self.update_score()
+
     def update_score(self):
-        self.write(f"Score: {self.score}", font=("Courier", 21, "normal"))
+        self.clear()        # clears the previous score from the turtle screen to avoid overlapping
+        self.write(f"SCORE: {self.score} HIGH SCORE: {self.high_score}", font=("Courier", 21, "normal"))
 
     def giveScore(self):
         self.score += 1     # score will increment by 1
-        self.clear()        # clears the previous score from the turtle screen to avoid overlapping
         self.update_score() # after the previous score has been cleared the score is updated
-

dcf512fa8c19c221cf3b5242f0e79e23eedaeec9
Omer Aqeel <93266569+OmerAqeel@users.noreply.github.com>
2022-08-24 01:26:33 +0300

    Update food.py

--- food.py
+++ food.py
@@ -11,12 +11,12 @@ class Food(Turtle):
         self.shape("circle")
         self.penup()
         self.shapesize(stretch_len= 0.5, stretch_wid= 0.5)
-        self.color("red")
+        self.color("orange")
         self.speed("fastest")
         self.goto(x=X,y=Y)      #The food will move to random points on the screen
 
     #As soon as the snake will eat the food the food will move to new position on the screen
     def move(self):
-        new_posX = random.randint(-270, 270) 
+        new_posX = random.randint(-270, 270)
         new_posY = random.randint(-270, 270)
         self.goto(x = new_posX, y = new_posY)
